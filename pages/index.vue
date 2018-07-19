664dac5cff17efde82cdb82a643e8dbab3a8fc01
Benoit <benoit.carquillat@gmail.com>
2018-07-19 18:28:41 +0200

    intro letter animation stroke

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <section class="container">
+  <section class="home">
     <div>
-      <h1>Hello</h1>
+      <div class="screen">
+        <object>  
+          <embed src="/letter.svg">
+        </object>
+
+          <nuxt-link to="/works">Works</nuxt-link>
+      </div>
     </div>
   </section>
 </template>
@@ -15,35 +21,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
